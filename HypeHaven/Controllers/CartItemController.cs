35fd2fb8a06e9c6528bc2310c6d98ec286714eac
Jakub Dzikowski <dzikowskijakub2001@gmail.com>
2023-11-03 23:23:04 +0100

    Integration with stripe & Checkout method completed

--- HypeHaven/Controllers/CartItemController.cs
+++ HypeHaven/Controllers/CartItemController.cs
@@ -4,6 +4,8 @@ using HypeHaven.Interfaces;
 using HypeHaven.models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Stripe.Checkout;
+//using Stripe.BillingPortal;
 using Stripe;
 
 namespace HypeHaven.Controllers
@@ -52,7 +54,7 @@ namespace HypeHaven.Controllers
 
             _cartItemRepository.Add(cartItem);
 
-            return RedirectToAction("Index", "Product");
+            return RedirectToAction("ViewCart");
         }
 
         [HttpGet]
@@ -61,6 +63,136 @@ namespace HypeHaven.Controllers
             var cartItems = await _cartItemRepository.GetAllForSpecifedUser();
             return View(cartItems);
         }
+
+        [HttpPost]
+        public async Task<IActionResult> CheckOut()
+        {
+            var currentUserId = _httpContextAccessor.HttpContext.User.GetUserId();
+            var cart = await _cartRepository.GetCartByUserIdAsync(currentUserId);
+            var cartItems = await _cartItemRepository.GetAllForSpecifedUser();
+
+            var domain = "https://localhost:7143/";
+
+            var options = new SessionCreateOptions
+            {
+                SuccessUrl = domain + "CartItem/OrderConfirmation",
+                CancelUrl = domain + "CartItem/OrderCancelled",
+                LineItems = new List<SessionLineItemOptions>(),
+                Mode = "payment",
+                Currency = "pln",  // Set the currency here
+            };
+
+            foreach (var item in cartItems)
+            {
+                // Calculate the unit amount based on the price and quantity in the smallest currency unit (grosze for PLN).
+                long unitAmount = (long)(item.Product.Price * 100); // Convert to grosze
+
+                var sessionListItem = new SessionLineItemOptions
+                {
+                    PriceData = new SessionLineItemPriceDataOptions
+                    {
+                        UnitAmount = unitAmount,  // Set the unit amount
+                        Currency = "pln",
+                        ProductData = new SessionLineItemPriceDataProductDataOptions
+                        {
+                            Name = item.Product.Name,
+                        },
+                    },
+                    Quantity = item.Quantity,
+                };
+
+                options.LineItems.Add(sessionListItem);
+            }
+
+            var service = new SessionService();
+            Session session = service.Create(options);
+
+            // Used for redirection
+            Response.Headers.Add("Location", session.Url);
+            return new StatusCodeResult(303);
+        }
+
+
+
+        /*
+                [HttpPost]
+                public async Task<IActionResult> CheckOut()
+                {
+                    var currentUserId = _httpContextAccessor.HttpContext.User.GetUserId();
+                    var cart = await _cartRepository.GetCartByUserIdAsync(currentUserId);
+                    var cartItems = await _cartItemRepository.GetAllForSpecifedUser();
+
+                    var domain = "https://localhost:7143/";
+
+                    var options = new SessionCreateOptions
+                    {
+                        SuccessUrl = domain + $"CartItem/OrderConfirmation",
+                        CancelUrl = domain + $"CartItem/OrderCancelled",
+                        LineItems = new List<SessionLineItemOptions>(),
+                        Mode = "payment",              
+                    };
+
+                    foreach (var item in cartItems)
+                    {
+                        var sessionListItem = new SessionLineItemOptions
+                        {
+                            PriceData = new SessionLineItemPriceDataOptions
+                            {
+                                UnitAmount = (long)item.Product.Price * item.Product.Quantity,
+                                Currency = "pln",
+                                ProductData = new SessionLineItemPriceDataProductDataOptions
+                                {
+                                    Name = item.Product.Name, // = item.Product.ToString(),
+                                },
+                            },
+                            Quantity = item.Quantity,
+                        };
+                        options.LineItems.Add(sessionListItem);
+                    }
+
+                    var service = new SessionService();
+                    Session session = service.Create(options);
+
+                    //used for redirection
+                    Response.Headers.Add("Location", session.Url);
+                    return new StatusCodeResult(303);
+
+                   *//* var options = new SessionCreateOptions
+                    {
+                        PaymentMethodTypes = new List<string>
+                        {
+                            "card",
+                        },
+                        LineItems = new List<SessionLineItemOptions>(),
+                        Mode = "payment",
+                        SuccessUrl = "https://localhost:5001/CartItem/CheckoutSuccess?session_id={{CHECKOUT_SESSION_ID}}",
+                        CancelUrl = "https://localhost:5001/CartItem/CheckoutCancel",
+                    };
+
+                    foreach (var item in cartItems)
+                    {
+                        options.LineItems.Add(new SessionLineItemOptions
+                        {
+                            PriceData = new SessionLineItemPriceDataOptions
+                            {
+                                UnitAmount = (long)item.Product.Price * 100,
+                                Currency = "pln",
+                                ProductData = new SessionLineItemPriceDataProductDataOptions
+                                {
+                                    Name = item.Product.Name,
+                                },
+                            },
+                            Quantity = item.Quantity,
+                        });
+                    }
+
+                    var service = new SessionService();
+                    Session session = service.Create(options);
+
+                    return Redirect(session.Url);
+                }*//*
+                   }
+        */
 
     }
 }
