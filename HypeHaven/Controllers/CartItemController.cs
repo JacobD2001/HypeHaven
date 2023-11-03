b41f737cfb1d60e23f3797ac5a51bccfee715cd8
Jakub Dzikowski <dzikowskijakub2001@gmail.com>
2023-11-03 23:52:26 +0100

    OrderConfirmation completed, checkout process with stripe completed

--- HypeHaven/Controllers/CartItemController.cs
+++ HypeHaven/Controllers/CartItemController.cs
@@ -76,15 +76,16 @@ namespace HypeHaven.Controllers
             var options = new SessionCreateOptions
             {
                 SuccessUrl = domain + "CartItem/OrderConfirmation",
-                CancelUrl = domain + "CartItem/OrderCancelled",
+                CancelUrl = domain + "CartItem/OrderFailed",
                 LineItems = new List<SessionLineItemOptions>(),
                 Mode = "payment",
-                Currency = "pln",  // Set the currency here
+                Currency = "pln",
+                CustomerEmail = User.Identity.Name
             };
 
             foreach (var item in cartItems)
             {
-                // Calculate the unit amount based on the price and quantity in the smallest currency unit (grosze for PLN).
+                // Calculate the unit amount based on the price and quantity in the smallest currency unit (grosze for PLN)
                 long unitAmount = (long)(item.Product.Price * 100); // Convert to grosze
 
                 var sessionListItem = new SessionLineItemOptions
@@ -107,92 +108,24 @@ namespace HypeHaven.Controllers
             var service = new SessionService();
             Session session = service.Create(options);
 
+            TempData["Session"] = session.Id;
             // Used for redirection
             Response.Headers.Add("Location", session.Url);
             return new StatusCodeResult(303);
         }
 
+        //no need for async
+        public IActionResult OrderConfirmation()
+        {
+            var service = new SessionService();
+            Session session = service.Get(TempData["Session"].ToString());
 
+            if (session.PaymentStatus == "paid")
+                return View("OrderConfirmed");
+            return View("OrderFailed");
+        }
 
-        /*
-                [HttpPost]
-                public async Task<IActionResult> CheckOut()
-                {
-                    var currentUserId = _httpContextAccessor.HttpContext.User.GetUserId();
-                    var cart = await _cartRepository.GetCartByUserIdAsync(currentUserId);
-                    var cartItems = await _cartItemRepository.GetAllForSpecifedUser();
-
-                    var domain = "https://localhost:7143/";
-
-                    var options = new SessionCreateOptions
-                    {
-                        SuccessUrl = domain + $"CartItem/OrderConfirmation",
-                        CancelUrl = domain + $"CartItem/OrderCancelled",
-                        LineItems = new List<SessionLineItemOptions>(),
-                        Mode = "payment",              
-                    };
-
-                    foreach (var item in cartItems)
-                    {
-                        var sessionListItem = new SessionLineItemOptions
-                        {
-                            PriceData = new SessionLineItemPriceDataOptions
-                            {
-                                UnitAmount = (long)item.Product.Price * item.Product.Quantity,
-                                Currency = "pln",
-                                ProductData = new SessionLineItemPriceDataProductDataOptions
-                                {
-                                    Name = item.Product.Name, // = item.Product.ToString(),
-                                },
-                            },
-                            Quantity = item.Quantity,
-                        };
-                        options.LineItems.Add(sessionListItem);
-                    }
-
-                    var service = new SessionService();
-                    Session session = service.Create(options);
-
-                    //used for redirection
-                    Response.Headers.Add("Location", session.Url);
-                    return new StatusCodeResult(303);
-
-                   *//* var options = new SessionCreateOptions
-                    {
-                        PaymentMethodTypes = new List<string>
-                        {
-                            "card",
-                        },
-                        LineItems = new List<SessionLineItemOptions>(),
-                        Mode = "payment",
-                        SuccessUrl = "https://localhost:5001/CartItem/CheckoutSuccess?session_id={{CHECKOUT_SESSION_ID}}",
-                        CancelUrl = "https://localhost:5001/CartItem/CheckoutCancel",
-                    };
-
-                    foreach (var item in cartItems)
-                    {
-                        options.LineItems.Add(new SessionLineItemOptions
-                        {
-                            PriceData = new SessionLineItemPriceDataOptions
-                            {
-                                UnitAmount = (long)item.Product.Price * 100,
-                                Currency = "pln",
-                                ProductData = new SessionLineItemPriceDataProductDataOptions
-                                {
-                                    Name = item.Product.Name,
-                                },
-                            },
-                            Quantity = item.Quantity,
-                        });
-                    }
-
-                    var service = new SessionService();
-                    Session session = service.Create(options);
-
-                    return Redirect(session.Url);
-                }*//*
-                   }
-        */
+   
 
     }
 }
